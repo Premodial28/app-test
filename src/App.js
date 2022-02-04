42dd9555019449bbb1e075ddb7f6a7370d8b90e4
Your Name <Premodial28>
2022-02-05 00:23:18 +0200

    dev

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is <code>My first </code> CI CD build
+          This is <code>My first </code> CI CD build Thats broken
         </p>
         <a
           className="App-link"
